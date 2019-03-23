8f7cec02cd9f2cd6b97a2189bbbd4e5d4bac8ed0
AndersonJS <ajs.ti@hotmail.com>
2019-03-23 17:03:16 -0400

    009-Tabuada Simples.

---
+++ 009 - Tabuada do número escolhido..py
@@ -0,0 +1,17 @@
+"""Faça um programa que leia um númro inteiro e mostre a sua tabuada."""
+
+num = int(input('Digite um número para obter a sua Tabuada: '))
+
+print('-'*15)
+print('Tabuada do {}.'.format(num))
+print('-'*15)
+print('{}x1 = {}'.format(num, num*1))
+print('{}x2 = {}'.format(num, num*2))
+print('{}x3 = {}'.format(num, num*3))
+print('{}x4 = {}'.format(num, num*4))
+print('{}x5 = {}'.format(num, num*5))
+print('{}x6 = {}'.format(num, num*6))
+print('{}x7 = {}'.format(num, num*7))
+print('{}x8 = {}'.format(num, num*8))
+print('{}x9 = {}'.format(num, num*9))
+print('{}x10= {}'.format(num, num*10))
