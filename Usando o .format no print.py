0574dc5d9615889542ec7858967eb8d426d71489
AndersonJS <ajs.ti@hotmail.com>
2019-03-22 20:23:06 -0400

    Usando o .format e limitando a casa decimal de um float.

---
+++ Usando o .format no print.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digte outro número: '))
+s = (n1 + n2)
+
+print('A soma do {} mais o {} é {}'.format(n1, n2, s))  # Para formata a quantidade de casas decimais de um float {:.2f}
