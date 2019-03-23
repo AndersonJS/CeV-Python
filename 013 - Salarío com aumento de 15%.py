1ce05ce1545a2c7b0e5d9cee9031410dec2a5426
AndersonJS <ajs.ti@hotmail.com>
2019-03-23 18:25:24 -0400

    013-Salarío com 15% de aumento.

---
+++ 013 - Salarío com aumento de 15%.py
@@ -0,0 +1,6 @@
+"""Faça um algoritmo que leia um Salarío de um fincionário e mostre seu novo salarío, com 15% de Aumento."""
+
+sal = float(input('Digite o seu salarío: '))
+aumento = sal+(15*sal)/100
+
+print('Seu salarío de R${} + 15% de aumento é de R${}'.format(sal, aumento))
