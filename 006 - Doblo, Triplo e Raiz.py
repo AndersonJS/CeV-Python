242972db97f655b3bf4c13b0ba0d2d4b4fd74870
AndersonJS <ajs.ti@hotmail.com>
2019-03-23 10:32:59 -0400

    Variveis com Doblo, triplo e a Raiz de um número.

--- 006 - Doblo, Triplo e Raiz.py
+++ 006 - Doblo, Triplo e Raiz.py
@@ -1,6 +1,6 @@
 """Faça um algoritmo que leia um número e mostre seu doblo, triplo e sua raiz."""
 
-num = int(input('Digite um número'))
+num = int(input('Digite um número: '))
 dob = num**2
 trip = num**3
 raiz = num**(1/2)
---
+++ 007 - Média com duas notas..py
@@ -0,0 +1,7 @@
+"""Desenvolva um programa que leia as duas notas de um aluno e mostre a sua média."""
+
+nota1 = float(input('Digite sua primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = 2/(nota1 + nota2)
+
+print('A sua média é {}.'.format(media))
