7846b347ecac157979176cfdf5778ed115195a25
AndersonJS <ajs.ti@hotmail.com>
2019-03-23 10:25:52 -0400

    Variveis com Doblo, triplo e a Raiz de um número.

---
+++ 006 - Doblo, Triplo e Raiz.py
@@ -0,0 +1,8 @@
+"""Faça um algoritmo que leia um número e mostre seu doblo, triplo e sua raiz."""
+
+num = int(input('Digite um número'))
+dob = num**2
+trip = num**3
+raiz = num**(1/2)
+
+print('O número {} tem como doblo o {} e o triplo é {} e sua Raiz Quadrada vale {}.'.format(num, dob, trip, raiz))
