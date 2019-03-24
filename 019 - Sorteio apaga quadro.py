169673aefd69a81677915c0f1faeed952de284d3
AndersonJS <ajs.ti@hotmail.com>
2019-03-24 18:31:45 -0400

    019-Sorteio para um aluno apagar o quadro, usando a lib random com choice e uma lista.

---
+++ 019 - Sorteio apaga quadro.py
@@ -0,0 +1,14 @@
+"""Um professor que sortear um dos seus quatro alunos para apaga o quadro. Faça um programa que ajude ele, lendo o nome
+deles e escrevendo o nome do escolhido."""
+
+from random import choice
+
+n1 = input('Primeiro aluno: ')
+n2 = input('Segundo aluno: ')
+n3 = input('Terceiro aluno: ')
+n4 = input('Quarto aluno: ')
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+
+print(' ')
+print('O aluno escolhido foi {}.'.format(escolhido))
