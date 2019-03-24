7398df9749b2c70dc14890f5ad5c9fc14ced40ad
AndersonJS <ajs.ti@hotmail.com>
2019-03-24 18:03:41 -0400

    018-Seno, coseno e tangente de um angulo e convertendo o grau para radiano com o radians.

--- 018 - Seno, Cosseno e Tangente de um ãngulo.py
+++ 018 - Seno, Cosseno e Tangente de um ãngulo.py
@@ -4,10 +4,10 @@ from math import sin, cos, tan, radians
 
 ang = float(input('Digite o ângulo: '))
 sen = sin(radians(ang))  # Não estava conseguindo pois tinha esquecido de converte o grau para radiano.
-cos = cos(radians(ang))
-tan = tan(radians(ang))
+cose = cos(radians(ang))
+tang = tan(radians(ang))
 
 print('Ângulo {}º'.format(ang))
 print('Seu Seno é {:.2f}'.format(sen))
-print('Seu coseno é {:.2f}'.format(cos))
-print(('Sua Tangente é {:.2f}'.format(tan)))
+print('Seu coseno é {:.2f}'.format(cose))
+print(('Sua Tangente é {:.2f}'.format(tang)))
