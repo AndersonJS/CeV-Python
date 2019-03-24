d0953d3cd5f4e88a08277e9358284a3c27595bb8
AndersonJS <ajs.ti@hotmail.com>
2019-03-24 18:47:59 -0400

    020-Ordem de apresentação de trabalho, com a lib random e o shuffle para embaralhar os nomes.

--- 020 - Sorteio para apresentação de trabalho.py
+++ 020 - Sorteio para apresentação de trabalho.py
@@ -8,7 +8,7 @@ n2 = input('Segundo aluno: ')
 n3 = input('Terceiro aluno: ')
 n4 = input('Quarto aluno: ')
 lista = [n1, n2, n3, n4]
-shuffle(lista)
+shuffle(lista)  # Embaralha os itens da lista.
 
 print('A ordem de apresentação será:')
 print(lista)
