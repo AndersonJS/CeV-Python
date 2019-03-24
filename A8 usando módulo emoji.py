76a6641fc16461076b1acb347d7863fa4290132f
AndersonJS <ajs.ti@hotmail.com>
2019-03-24 13:45:15 -0400

    Aula 8 que foi abordagem sobre o import e a instação de módulos e o uso da biblioteca emoji.

--- A8 usando módulo emoji.py
+++ A8 usando módulo emoji.py
@@ -1,5 +1,6 @@
 import emoji
 
-print(emoji.emojize('Olá Mundo :earth_americas:', use_aliases=True))
-print(emoji.emojize('Python :snake:', use_aliases=True))  # Tive de ler e ver uns exemplo no pypi para assimilar
-print(emoji.emojize('Tem amo :sparkling_heart:', use_aliases=True))
+print('-'*12)
+print(emoji.emojize('Olá Mundo! :earth_americas:', use_aliases=True))
+print(emoji.emojize('Python. :snake:', use_aliases=True))  # Tive de ler e ver uns exemplo no pypi para assimilar
+print(emoji.emojize('Tem amo. :sparkling_heart:', use_aliases=True))
