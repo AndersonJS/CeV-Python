d0b950292e74a4e5d119d9c836a72d883df103c4
AndersonJS <ajs.ti@hotmail.com>
2019-03-24 13:56:25 -0400

    014-ºC convertido para ºF

---
+++ 014 - Conversor de Temperatura.py
@@ -0,0 +1,6 @@
+"""Escreva um programa que converta uma temperatura digitada em ºC para ºF"""
+
+c = int(input('Digite a Temperatura em ºC: '))
+f = (c*9/5)+32
+
+print('{}ºC e igual a {}ºF.'.format(c, f))
